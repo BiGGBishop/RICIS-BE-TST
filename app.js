29ee91c8f31a8b6e70c15b84675ed962d1da1fba
lawalstacks <quazeemastic@gmail.com>
2025-01-14 21:36:37 +0000

    include bordy parser for json and doc upload to server [server.js]

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require("express");
 require("dotenv").config();
 const morgan = require("morgan");
+const bodyParser = require("body-parser")
 
 const app = express();
 app.use(morgan('combined'))
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+
+app.use(express.json())
 
 require("./operations/routes")(app);
 // require("./operations/db")(app);

fa467387615c8c4d240e64a3c8482bf48588c3ee
BiGGBishop <oladimejid@gmail.com>
2025-03-10 17:02:52 +0100

    [Function] Chaeck payment status

--- routes/users.js
+++ routes/users.js
@@ -19,6 +19,7 @@ router.post("/classification-with-merge",authToken,usersController.getClassifica
 router.put("/application/msg/add", authToken, asyncHandler(usersController.addMsgToApplication)); 
 router.get("/payment/token",authToken, asyncHandler(usersController.getPaymentToken));
 router.post("/initiate-payment", authToken, asyncHandler(usersController.makePayment));
+router.get("/payment-status", authToken, asyncHandler(usersController.checkTransactionStatus));
 router.get("/my-applications",authToken,asyncHandler(usersController.getAllUserForms));
 router.get("/my-applications-certificates",authToken,asyncHandler(usersController.getAllUserFormsWithCertificate));
 

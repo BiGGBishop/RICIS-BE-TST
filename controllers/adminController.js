c1e309b0f61c707d309f8f6103222c02b345723a
BiGGBishop <oladimejid@gmail.com>
2025-03-13 11:39:53 +0100

    Function: Pagination for applications

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -423,6 +423,10 @@ exports.getAllUsersForms = async (req, res) => {
     status: data.STATUS,
     message: data.MESSAGE,
     data: data.DATA,
+    totalCount: data.totalCount,
+    totalPages: data.totalPages,
+    page: data.page,
+    limit: data.limit,
   });
 };
 

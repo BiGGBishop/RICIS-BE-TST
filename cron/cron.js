01501cfa4ca9eba86f0e32ac7c156cab8f69e430
lawalstacks <quazeemastic@gmail.com>
2025-03-26 10:33:17 +0000

    cron

--- cron/cron.js
+++ cron/cron.js
@@ -3,7 +3,7 @@ const https = require("https")
 
 const URL = "https://ricis-be-tst-tn6m.onrender.com";
 
-const job = new cron.CronJob("* 14 * * * *", function () {
+const job = new cron.CronJob("*/20 * * * *", function () {
  https
   .get(URL, (res) => {
    if (res.statusCode === 200) {

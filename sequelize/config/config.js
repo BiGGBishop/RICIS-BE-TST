bf616be01349e7695d4d48ed0140aaf202775704
BiGGBishop <oladimejid@gmail.com>
2025-05-23 16:40:23 +0100

    Config correction

--- sequelize/config/config.js
+++ sequelize/config/config.js
@@ -1,42 +1,14 @@
-const {
-  SQLDB_URL,
-  ENVIRONMENT,
-  
-} = require("../../config/envConfig");
-
-
-console.log({
-  ENVIRONMENT,
-  SQLDB_URL, 
-  
-})
-
 module.exports = {
 
   development: {
-    // url: SQLDB_URL,
-    url: 'postgresql://neondb_owner:N7JjrKpB9swo@ep-round-base-a8cgesqx.eastus2.azure.neon.tech/neondb?sslmode=require',
-    // url: 'postgresql://neondb_owner:npg_BMR2afy0IcrV@ep-sparkling-mode-a4zbnqga-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require',
+    // url: 'postgresql://neondb_owner:N7JjrKpB9swo@ep-round-base-a8cgesqx.eastus2.azure.neon.tech/neondb?sslmode=require',
+    url: 'postgresql://ricis_user:ricisDBpass@localhost:5432/ricis_db',
     dialect: "postgres",
     dialectOptions:
-      // ENVIRONMENT === "production" || ENVIRONMENT === "staging"
-        // ? 
         {
             ssl: {
               rejectUnauthorized: false,
             },
           }
-        // : {},
   }, 
-  // test: {
-  //   username: "root",
-  //   password: null,
-  //   database: "database_test",
-  //   host: "127.0.0.1",
-  //   dialect: "postgres",
-  // },
-  // production: {
-  //   url: "127.0.0.1",
-  //   dialect: "postgres",
-  // },
 };

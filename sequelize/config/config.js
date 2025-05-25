8b152567afaa5a615d5351c0090a977d46529383
BiGGBishop <oladimejid@gmail.com>
2025-05-25 18:23:22 +0100

    corrections

--- sequelize/config/config.js
+++ sequelize/config/config.js
@@ -2,7 +2,8 @@ module.exports = {
 
   development: {
     // url: 'postgresql://neondb_owner:N7JjrKpB9swo@ep-round-base-a8cgesqx.eastus2.azure.neon.tech/neondb?sslmode=require',
-    url: 'postgresql://ricis_user:ricisDBpass@localhost:5432/ricis_db',
+    // url: 'postgresql://ricis_user:ricisDBpass@localhost:5432/ricis_db',
+    url: 'postgresql://neondb_owner:npg_BMR2afy0IcrV@ep-sparkling-mode-a4zbnqga-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require',
     dialect: "postgres",
     dialectOptions:
         {
---
+++ sequelize/migrations/20250101620253-new-admin-detail.js
@@ -0,0 +1,32 @@
+'use strict';
+const bcrypt = require("bcryptjs")
+
+//const { userRepo } = require("../../repositories/userRepo");
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /*const adminRole = await userRepo.findRole({ name: 'admin' });
+    if (!adminRole) {
+      console.error("Admin role not found. Please create the 'admin' role first.");
+      return;
+    }*/
+      const hashedPassword = bcrypt.hashSync('FMT-ech123', 10);
+
+    await queryInterface.bulkInsert('adminstaffs', [{
+      email: 'oladimejid@gmail.com',
+      password: hashedPassword,
+      full_name: 'Default Admin',
+      ref_number: "DEVTHING",
+      userroleId: 1,
+      user_status: 'approved',
+      payment_status: 'paid',
+      createdAt: new Date(), 
+      updatedAt: new Date()  
+    }]);
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('adminstaffs', { email: 'admin@ricis.com' });
+  }
+};
